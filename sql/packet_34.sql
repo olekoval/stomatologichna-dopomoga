d36a2f2d648234a0bd3d770b9fdf4d04d529ae6c
Oleh <314koval.ok@gmail.com>
2024-07-03 12:42:21 +0300

    first formated

--- sql/packet_34.sql
+++ sql/packet_34.sql
@@ -1,6 +1,7 @@
  
  
- FROM analytics.rds_pmg_events_2024 AS e
-       INNER JOIN analytics.rds_pmg_events_checks_2024 AS eh ON e.id = eh.id             
+  FROM analytics.rds_pmg_events_2024 AS e
+       JOIN analytics.rds_pmg_events_checks_2024 AS eh ON e.id = eh.id             
  WHERE eh.packet_number = '34'
    AND eh.is_correct
+   AND age_years < 18
