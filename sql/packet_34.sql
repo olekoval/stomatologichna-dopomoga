ca9a43d997edd4582b0f3558a1152b3028ecc171
Oleh <314koval.ok@gmail.com>
2024-07-03 16:01:16 +0300

    count_avg_patient_result

--- sql/packet_34.sql
+++ sql/packet_34.sql
@@ -1,7 +1,15 @@
+SELECT COUNT(patient_repetitions) AS кiлькiсть_унiкальних_пацієнтів,
+       AVG(patient_repetitions) AS середня_кiлькiсть_емз_на_пацієнта
+  FROM (
+    SELECT COUNT(*) AS patient_repetitions 
+      FROM analytics.rds_pmg_events_checks_2024 AS ech
+           JOIN analytics.rds_pmg_events_2024 AS e ON e.id = ech.id             
+     WHERE ech.packet_number = '34'
+       AND ech.is_correct
+       AND ech.is_payment
+       AND e.age_years < 18
+       AND EXTRACT(MONTH FROM e.starts) < 7
+     GROUP BY e.patient_id
+     ) AS t;
  
- 
-  FROM analytics.rds_pmg_events_2024 AS e
-       JOIN analytics.rds_pmg_events_checks_2024 AS eh ON e.id = eh.id             
- WHERE eh.packet_number = '34'
-   AND eh.is_correct
-   AND age_years < 18
+
